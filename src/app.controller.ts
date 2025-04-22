e7b3f6fa7711ec6b82b170c46528652a9cd6a836
Rajnish Kumar <rajnish6633@gmail.com>
2025-04-22 08:55:11 +0530

    Test CI/CD - updated log

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {
-    console.log('🚀App is starting...');
+    console.log('🚀 App is starting...');
   }
 
   @Get()
